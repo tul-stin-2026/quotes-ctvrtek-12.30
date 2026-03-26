b3c84e2f5acf030e54c16fbeb02a144906722d23
Pavel Tyl <pavel.tyl@gmail.com>
2026-03-26 13:44:10 +0100

    Endpoints

--- src/main/java/cz/tul/stin/paveltyl/quotes/QuotesApplication.java
+++ src/main/java/cz/tul/stin/paveltyl/quotes/QuotesApplication.java
@@ -2,6 +2,8 @@ package cz.tul.stin.paveltyl.quotes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class QuotesApplication {
@@ -10,4 +12,8 @@ public class QuotesApplication {
         SpringApplication.run(QuotesApplication.class, args);
     }
 
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
